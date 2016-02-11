2e314c12451cc714b9544be3956d6b56aadd2ef6
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-02-11 14:31:57 +0530

    hapijs

---
+++ index.js
@@ -0,0 +1,71 @@
+const Hapi = require('hapi');
+const Path = require('path');
+const Inert  = require('inert');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.register(Inert, () => {});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: '.',
+            redirectToSlash: true,
+            index: true,
+            listing: true
+        }
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    console.log('Server running at:', server.info.uri);
+});
+
+/*server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!'),
+        directory: {
+            path: './',
+            listing: true
+        }
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, (err) => {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(() => {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});*/
