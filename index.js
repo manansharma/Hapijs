6a94dfb7f8f89d51e984e2ae69e3a786edfc15e5
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-02-18 12:18:38 +0530

    ïnert test

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
-const hapi = require('hapi');
+const Path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
 
-const server = new hapi.Server();
-
-server.connection({
-  host: 'localhost',
-  address: '127.0.0.1',
-  port: 3000,
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
 });
+server.connection({ port: 3000 });
 
-server.register({
-  register: require('hapi-server-session'),
-  options: {
-    cookie: {
-      isSecure: false,
-      isHttpOnly: false
-    },
-  },
-}, function (err) { if (err) { throw err; } });
+server.register(Inert, () => {});
+
+/*server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: '.',
+            redirectToSlash: true,
+            index: true
+        }
+    }
+});*/
 
 server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    request.session.views = request.session.views + 1 || 1;
-    reply('Views: ' + request.session.views);
-  },
+    method: 'GET',
+    path: '/documents/{user}/{file}',
+    handler: function(request, reply) {
+        var path = Path.join(request.params.user, request.params.file);
+        return reply.file(path);
+    }
 });
 
-server.start();
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    console.log('Server running at:', server.info.uri);
+});
+
+/*server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+
+
+    server.route({
+        method: 'GET',
+        path: '/documents/{user}/{file}',
+        handler: function(request, reply) {
+            var path = Path.join(request.params.user, request.params.file);
+            return reply.file(path);
+        }
+    });
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+
+        console.log('Server running at:', server.info.uri);
+    });
+});*/
