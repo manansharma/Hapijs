0a8e440c8c5a256b6eb662b2ec5f8c049ac1d547
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-02-23 14:47:33 +0530

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.register({
   register: require('hapi-server-session'),
   options: {
     cookie: {
-      isSecure: false,
+      isSecure: true,
       isHttpOnly: false
     },
     expiresIn: 9000000,
