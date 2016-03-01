29cd198e5bf628b69c442f62f32341f4e2932851
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-01 13:12:23 +0530

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.register({
   options: {
     cookie: {
       isSecure: false,
-      isHttpOnly: true
+      isHttpOnly: false
     },
     expiresIn: 12000000000000000000000,
     //key: cryptiles.randomString(16);
