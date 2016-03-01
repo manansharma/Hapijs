2760b6fe729563967f13c8d7dcce0662ab3d12ed
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-01 12:31:18 +0530

    test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.register({
       isSecure: true,
       isHttpOnly: true
     },
-    expiresIn: 9000000,
+    //expiresIn: 9000000,
     key: cryptiles.randomString(16);
   },
 }, function (err) { if (err) { throw err; } });
