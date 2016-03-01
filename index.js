7092b263252d7bd9e52f7726c44759f55840305c
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-01 16:22:31 +0530

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const hapi = require('hapi');
 const server = new hapi.Server();
 var cryptiles = require('cryptiles');
-var bcryptObj = require('bcrypt');
-const Basic = require('hapi-auth-basic');
+var bcrypt = require('bcrypt');
+
 
 server.connection({
   host: 'localhost',
@@ -10,25 +10,18 @@ server.connection({
   port: 3000,
 });
 
-const users = {
-    john: {
-        username: 'john',
-        password: 'secret',   // 'secret'
-        name: 'John Doe',
-        id: '2133d32a'
-    }
-};
+bcrypt.genSalt(10, function(err, salt) {
+ bcrypt.hash('B4c0/\/', salt, function(err, hash) {
+ // Store hash in your password DB.
+ });
+});
 
-const validate = function (request, username, password, callback) {
-    const user = users[username];
-    if (!user) {
-        return callback(null, false);
-    }
+// Load hash from your password DB.
+bcrypt.compare('B4c0/\/', hash, function(err, res) {
+ // res == true
+});
 
-    /*Bcrypt.compare(password, user.password, (err, isValid) => {
-        callback(err, isValid, { id: user.id, name: user.name });
-    });*/
-};
+
 
 //Hapi.hs Server Session Test
 /*server.register({
@@ -41,7 +34,7 @@ const validate = function (request, username, password, callback) {
     expiresIn: 900000,
     //key: cryptiles.randomString(16);
   },
-}, function (err) { if (err) { throw err; } });
+}, function (err) { if (err) { throw err; } });*/
 
 
 
@@ -53,21 +46,9 @@ server.route({
     request.session.views = request.session.views + 1 || 1;
     reply('Views: ' + request.session.views);
   },
-});*/
+});
 
-server.register(Basic, (err) => {
-    server.auth.strategy('simple', 'basic', { validateFunc: validate });
-    server.route({
-        method: 'GET',
-        path: '/',
-        config: {
-            auth: 'simple',
-            handler: function (request, reply) {
-                reply('hello, ' + request.auth.credentials.name);
-            }
-        }
-    });
-  });
+
 
 
 
