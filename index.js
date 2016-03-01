2f3d76a438d86ba6ed694e2df9b8427949302b83
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-01 20:50:07 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hapi = require('hapi');
 const server = new hapi.Server();
 var cryptiles = require('cryptiles');
-var bcrypt = require('bcrypt');
+var Bcrypt = require('bcrypt');
 const Basic = require('hapi-auth-basic');
 
 
@@ -12,36 +12,48 @@ server.connection({
   port: 3000,
 });
 
-const users = {
-    john: {
-        username: 'john',
-        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
-        name: 'John Doe',
+// Create a in memory collections of users
+var users = {
+    jane: {
+        username: 'jane',
+        password: '$2a$10$XPk.7lupEzBSHxUg/IavSuIKmwmpBbW0NfCL8q0ZfHXUPXTtbhmNK',   // 'password'
+        name: 'Jane Doe',
         id: '2133d32a'
     }
 };
+// Create hash for string 'password'
+Bcrypt.hash('password', null, null, function (err, hash) {
+  console.log(err, hash);
+});
 
-//bcrypt.genSalt(10, function(err, salt) {
- bcrypt.hash( request.payload.password, request.payload.hello, function(err, hash) {
-   hashsave = hash,
- });
-//});
-
-const validate = function (request, username, password, callback) {
-    const user = users[username];
+// Create a validation function for strategy
+var validate = function (username, password, callback) {
+    var user = users[username];
     if (!user) {
         return callback(null, false);
     }
-
-    bcrypt.compare(request.payload.password, request.payload.hello, (err, isValid) => {
+    Bcrypt.compare(password, user.password, function (err, isValid) {
         callback(err, isValid, { id: user.id, name: user.name });
     });
 };
-/*bcrypt.compare(request.payload.password, "T20", function(err, res) {
- // res == true
-});*/
 
+// Add the basic-auth plug-in
+server.register(require('hapi-auth-basic'), function (err) {
+    server.auth.strategy('simple', 'basic', { validateFunc: validate });
+});
 
+// Add a simple route
+server.route({
+    method: 'GET',
+    path: '/',
+    config: { auth: 'simple' },
+    handler: function (request, reply) {
+        var name = request.auth.credentials.name
+        reply('hello ' + name);
+    }
+});
+
+server.start();
 
 //Hapi.hs Server Session Test
 /*server.register({
@@ -70,21 +82,3 @@ const validate = function (request, username, password, callback) {
           }
 });
 server.start();*/
-
-server.register(Basic, (err) => {
-    server.auth.strategy('simple', 'basic', { validateFunc: validate });
-    server.route({
-        method: 'GET',
-        path: '/',
-        config: {
-            auth: 'simple',
-            handler: function (request, reply) {
-                reply('hello, ' + request.auth.credentials.name);
-            }
-        }
-    });
-
-    server.start(() => {
-        console.log('server running at: ' + server.info.uri);
-    });
-});
