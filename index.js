c3cb9309df816d36a52df786d0f2e8ba50905b64
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-01 16:44:55 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const hapi = require('hapi');
 const server = new hapi.Server();
 var cryptiles = require('cryptiles');
 var bcrypt = require('bcrypt');
-var hashsave;
+const Basic = require('hapi-auth-basic');
+
 
 
 server.connection({
@@ -11,16 +12,34 @@ server.connection({
   port: 3000,
 });
 
-bcrypt.genSalt(10, function(err, salt) {
+const users = {
+    john: {
+        username: 'john',
+        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+        name: 'John Doe',
+        id: '2133d32a'
+    }
+};
+
+/*bcrypt.genSalt(10, function(err, salt) {
  bcrypt.hash('B4c0/\/', salt, function(err, hash) {
    hashsave = hash,
  });
-});
+});*/
 
-// Load hash from your password DB.
-bcrypt.compare('B4c0/\/', hashsave, function(err, res) {
+const validate = function (request, username, password, callback) {
+    const user = users[username];
+    if (!user) {
+        return callback(null, false);
+    }
+
+    bcrypt.compare('B4c0/\/', user.password, (err, isValid) => {
+        callback(err, isValid, { id: user.id, name: user.name });
+    });
+};
+/*bcrypt.compare('B4c0/\/', hashsave, function(err, res) {
  // res == true
-});
+});*/
 
 
 
@@ -40,7 +59,7 @@ bcrypt.compare('B4c0/\/', hashsave, function(err, res) {
 
 
 
-server.route({
+/*server.route({
   method: 'GET',
   path: '/',
   config: {
@@ -50,9 +69,22 @@ server.route({
               }
           }
 });
+server.start();*/
 
+server.register(Basic, (err) => {
+    server.auth.strategy('simple', 'basic', { validateFunc: validate });
+    server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+            auth: 'simple',
+            handler: function (request, reply) {
+                reply('hello, ' + request.auth.credentials.name);
+            }
+        }
+    });
 
-
-
-
-server.start();
+    server.start(() => {
+        console.log('server running at: ' + server.info.uri);
+    });
+});
