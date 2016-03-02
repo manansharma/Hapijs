6090f9e32fa6230d5d77d5d29dad2e72195bd53f
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-02 12:21:05 +0530

    test

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ server.register(require('hapi-auth-basic'), function (err) {
 //Case 2 - Multiple plugin register scenario
 server.register([{
       register: require('hapi-server-session'),
-      options: {cookie:{},expiresIn: 900000}
+      options: {cookie:{isSecure: false},expiresIn: 900000}
   }, {
       register: require('hapi-auth-basic'),
       options: {}
