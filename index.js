9e780e868b62e50367ed7b9fe0cc41436c8e51f4
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-07 11:22:30 +0530

    test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const hapi = require('hapi');
 const server = new hapi.Server();
 var cryptiles = require('cryptiles');
-//var Bcrypt = require('bcrypt');
+var Bcrypt = require('bcrypt');
 const Basic = require('hapi-auth-basic');
 const Inert = require('inert');
-var crypto = require('crypto');
+//var crypto = require('crypto');
 
 'use strict';
 
@@ -92,8 +92,8 @@ server.route({
     handler: function (request, reply) {
         //var name = request.auth.credentials.name
         //reply('hello ' + name);
-        //reply(Bcrypt.hashSync(request.params.password, request.params.hash));
-        reply(request.params.password, request.params.hash);
+      reply(Bcrypt.hashSync(request.params.password, request.params.hash));
+        //reply(request.params.password, request.params.hash);
 
     }
 });
