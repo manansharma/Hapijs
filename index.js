af80e142deabadd5360c19a5519973778c4d4d68
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-17 16:03:33 +0530

    test

--- index.js
+++ index.js
@@ -1,53 +1,137 @@
+const hapi = require('hapi');
+const server = new hapi.Server();
+var cryptiles = require('cryptiles');
+var Bcrypt = require('bcrypt');
+const Basic = require('hapi-auth-basic');
+const Inert = require('inert');
+var crypto = require('crypto');
+
 'use strict';
 
-var Hapi = require('hapi');
-var Joi = require('joi');
-var myBcrypt = require('bcrypt');
-
-var server = new Hapi.Server();
-server.app.key = 'secret_app_value_102';
 server.connection({
-  port: 3000
+  host: 'localhost',
+  address: '127.0.0.1',
+  port: 3000,
+});
+
+//Case 1 - Simple single plugin register scenario
+server.register({
+  register: require('hapi-server-session'),
+  options: {
+    cookie: {
+      isSecure: true,
+      isHttpOnly: true
+    },
+    expiresIn: 900000000000000000000000000000000000000000,
+    //key: cryptiles.randomString(16);
+  },
+}, function (err) { if (err) { throw err; } });
+
+
+
+//Case 2 - Multiple plugin register scenario
+//Test trigger for Hapi Server Session
+/*server.register([/*{
+      register: require('hapi-server-session'),
+      options: {cookie:{isSecure: true, isHttpOnly: true},expiresIn: 90000000000000000000000000000}
+  }, {
+      register: Inert,
+      options: {}
+  }], (err) => {
+});*/
+
+//server.register(Inert, () => {});
+server.register(Inert, function () {});
+
+//Test trigger for Hapi Inert Dynamic Files
+/*server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: function (request, reply) {
+        var path = Path.join(request.params.user, request.params.file);
+        return reply.file(path);
+    }
+});*/
+
+//*********************************************************************************************************
+//**********************Test trigger for Hapi Inert Directory Listing Suppression*************************************************
+//*******************************************************************************************************
+server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler:  {
+      directory: {
+          path: '../test.html',
+          listing: true
+      }
+    }
+});
+//******************************************************************************************************
+//**************************End of trigger for Hapi Inert Directory Listing Suppression****************************************
+//*****************************************************************************************************
+
+
+
+
+//*********************************************************************************************************
+//**********************Test trigger for Hapi Bcrypt Salt*************************************************
+//*******************************************************************************************************
+server.route({
+    method: 'GET',
+    path: '/test/{password*}',
+    //config: { auth: 'simple' },
+    handler: function (request, reply) {
+        //Hapi Bcrypt Salt Trigger
+        reply(Bcrypt.hashSync(request.params.password, request.params.hash));
+    }
+});
+//******************************************************************************************************
+//**************************End of trigger for Hapi Bcrypt Salt****************************************
+//*****************************************************************************************************
+
+
+
+
+
+
+
+//*******************************************************************************************************
+//********************  Start of trigger for Hapi Adaptive One Way Hash Password Storage *****************
+//*******************************************************************************************************
+// Create a in memory collections of users
+var users = {
+    jane: {
+        username: 'jane',
+        password: '$2a$10$XPk.7lupEzBSHxUg/IavSuIKmwmpBbW0NfCL8q0ZfHXUPXTtbhmNK',   // 'password'
+        name: 'Jane Doe',
+        id: '2133d32a'
+    }
+};
+// Create a validation function for strategy
+var validate = function (username, password, callback) {
+    var user = users[username];
+    if (!user) {
+        return callback(null, false);
+    }
+
+};
+
+// Add the basic-auth plug-in
+server.register(require('hapi-auth-basic'), function (err) {
+    server.auth.strategy('simple', 'basic', { validateFunc: validate });
 });
 
 server.route({
-  method: 'POST',
-  path: '/negative/bcrypt/1/{password*}',
-  config: {
-    validate: {
-      params: {
-        password: Joi.string().max(128).min(8).alphanum()
-      }
-    },
+    method: 'GET',
+    path: '/test/{password*}',
+    config: { auth: 'simple' },
     handler: function (request, reply) {
-      myBcrypt.hash(request.params.password, null, null, function (err, hash) {
-        if (err) {
-          return reply(err);
-        }
-        reply(hash);
-      });
+        reply(request.params.hash);
     }
-  }
 });
+//********************************************************************************************
+//***************End of trigger for Hapi Adaptive One Way Hash Password Storage***************
+//********************************************************************************************
 
-server.route({
-  method: 'POST',
-  path: '/negative/bcrypt/2/{password*}',
-  config: {
-    validate: {
-      params: {
-        password: Joi.string().max(128).min(8).alphanum()
-      }
-    },
-    handler: function (request, reply) {
-      var hash = 'Hello World';
-      var i = 0;
-      do {
-        hash += i;
-      } while (++i < 10);
-      reply(myBcrypt.hashSync(request.params.password, hash));
-    }
-  }
-});
 
-server.start(function () {});
+server.start();
