47acb26c9594c7f5ebd6f1d2cbdbc99ef784c1b4
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-03-17 15:40:40 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var Hapi = require('hapi');
 var Joi = require('joi');
-var myBcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 
 var server = new Hapi.Server();
 server.app.key = 'secret_app_value_102';
@@ -12,7 +12,7 @@ server.connection({
 
 server.route({
   method: 'POST',
-  path: '/negative/bcrypt/3/{password*}',
+  path: '/positive/bcrypt/1/{password*}',
   config: {
     validate: {
       params: {
@@ -20,7 +20,75 @@ server.route({
       }
     },
     handler: function (request, reply) {
-      reply(myBcrypt.hashSync(request.params.password, request.params.hash));
+      //var salt1 = bcrypt.genSaltSync(10); // param is optional
+      reply(bcrypt.hashSync(request.params.password, bcrypt.genSaltSync(10)));
+    }
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/positive/bcrypt/2/{password*}',
+  config: {
+    validate: {
+      params: {
+        password: Joi.string().max(128).min(8).alphanum()
+      }
+    },
+    handler: function (request, reply) {
+      bcrypt.genSalt(10, function (err, salt) { // first param is optional
+        if (err) {
+          return reply(err);
+        }
+        reply(bcrypt.hashSync(request.params.password, salt));
+      });
+    }
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/positive/bcrypt/3/{password*}',
+  config: {
+    validate: {
+      params: {
+        password: Joi.string().max(128).min(8).alphanum()
+      }
+    },
+    handler: function (request, reply) {
+      bcrypt.genSalt(function (err, res) {
+        if (!err) {
+          reply(bcrypt.hashSync(request.params.password, res));
+        } else {
+          reply("Internal Error");
+        }
+      });
+    }
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/positive/bcrypt/4/{password*}',
+  config: {
+    validate: {
+      params: {
+        password: Joi.string().max(128).min(8).alphanum()
+      }
+    },
+    handler: function (request, reply) {
+      bcrypt.genSalt(function (err, res) {
+        if (!err) {
+          bcrypt.hash(request.params.password, res, null, function (err, hash) {
+            if (err) {
+              return reply(err);
+            }
+            reply(hash);
+          });
+        } else {
+          reply("Internal Error");
+        }
+      });
     }
   }
 });
